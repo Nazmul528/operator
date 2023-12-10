fb44b2b019d5aa210f1e3a0627db1f6742bf1cdd
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2023-12-10 21:42:58 +0600

    Create area of circle.c

---
+++ area of circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float radies,area;
+    printf("Enter the radies ");
+    scanf("%f",&radies);
+    area =M_PI*radies*radies;
+    printf("the circle area :%.3f",area);
+}
