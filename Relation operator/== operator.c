690f0cde64dd72d72a53c7fefcfb889142e84188
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2023-12-18 08:11:13 +0600

    Create == operator.c

---
+++ Relation operator/== operator.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the charter :");
+    scanf("%c",&a);
+    if(a='a')
+    printf("the vowel");
+    else if (a=='e')
+    printf("the vowel");
+    else if (a=='i')
+    printf("the vowel");
+    else if (a=='o')
+    printf("the vowel");
+    else if (a=='u')
+    printf("the vowel");
+    else if (a=='y')
+    printf("the special vowel");
+    else
+    printf(" the consonant");
+    return 0;
+}    
