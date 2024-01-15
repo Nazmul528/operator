a1b2eade0216789808d2d7f00954bdd8524eb32d
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2024-01-15 21:58:05 +0600

    Create Lergh number.c

---
+++ conditional operator/Lergh number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num2,num3,lergh;
+    printf("Enter the two number ");
+    scanf("%d %d",&num2,&num3);
+    lergh =  num2>num3 ? num2 :num3;
+    printf("lergh number %d",lergh);
+
+}
