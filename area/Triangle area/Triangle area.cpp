9e49a262532c05bc4f454573b16730504c1dd862
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2024-01-28 15:20:14 +0600

    Create Triangle area.cpp

---
+++ area/Triangle area/Triangle area.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+
+{   double base,height,area;;
+    while(4)
+ {
+    cout <<"Enter the base :";
+    cin>>base;
+    cout<<"Enter the height :";
+    cin>>height;
+    area=(double)1/2*base*height;
+    cout<<"the triangle area :"<<area<<"\n\n";
+ }
+    getch();
+}
