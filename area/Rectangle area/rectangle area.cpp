a4c8c5d4a4299bf3d1776825d40ffddbf8efcd5d
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2024-07-08 11:30:11 +0600

    Create rectangle area.cpp

---
+++ area/Rectangle area/rectangle area.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    double width,hight,area;
+    cout <<"Enter the width :";
+    cin >> width;
+    cout <<"Enter the hight :";
+    cin >> hight;
+    area=width *hight;
+    cout <<"area of Rectangle :"<<area;
+   getch();
+}
