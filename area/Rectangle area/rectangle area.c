059816495011215ea636d00488c950b3fcdb339c
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2024-07-08 11:37:02 +0600

    Create rectangle area.c

---
+++ area/Rectangle area/rectangle area.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    double width, hight, area;
+    printf("Enter the width and height :");
+    scanf("%lf %lf",&width,&hight);
+    area=width*hight;
+    printf("area of rectangle :%lf",area);
+}
