7be14b79c81814391b9c241a11f8dd2fc45b668b
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2024-07-08 15:34:30 +0600

    Create Ellipse area.cpp

---
+++ area/Ellipse area/Ellipse area.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    double a,b,R=3.1416,area;
+    cout <<"Enter the  a and d lanth: :";
+    cin >> a>>b;
+    area=R*a*b;
+    cout <<"area of Ellipse :"<<area;
+    getch();
+    
+}
