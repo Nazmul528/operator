ac21999eeb6a654fd6fa40e7f060d259e6b0154b
Nazmul 528 -60-N <151040875+Nazmul528@users.noreply.github.com>
2024-07-10 15:18:19 +0600

    Create special operator(, * sizeof()).cpp

---
+++ Special operator/special operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    int sum =(x=10,y=30,x+y);
+    cout<<sum<<endl;
+    char name [20];
+    int s;
+    int c=sizeof (name);
+    cout<<c<<endl;
+    int p= sizeof(s);
+    cout << p << endl;
+}
